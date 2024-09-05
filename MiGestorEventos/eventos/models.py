e8583439cd83cadfba2e69d4fc753a67710805fa
Quake <camilo1356z@gmail.com>
2024-09-05 11:49:09 -0500

    configuracion

--- MiGestorEventos/eventos/models.py
+++ MiGestorEventos/eventos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Organizador(models.Model):
+    nombre = models.CharField(max_length=100)
+    contacto = models.EmailField()
+
+    def __str__(self):
+        return self.nombre
+
+class Evento(models.Model):
+    nombre = models.CharField(max_length=100)
+    fecha = models.DateTimeField()
+    organizador = models.ForeignKey(Organizador, on_delete=models.CASCADE, related_name='eventos')
+
+    def __str__(self):
+        return self.nombre
