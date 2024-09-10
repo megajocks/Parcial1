7a8677e946cd7a5dbe0921f3c65e671d6fae1d92
Quake <camilo1356z@gmail.com>
2024-09-10 15:51:59 -0500

    mas errores
    procedentes de puntos pertenecientes a eventos en views y url de la carpeta de la app

--- MiGestorEventos/eventos/urls.py
+++ MiGestorEventos/eventos/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
-from .views import OrganizadorListView, OrganizadorCreateView, EventoUpdateView
+from .views import OrganizadorListView, OrganizadorCreateView, EventoUpdateView, EventoCreateView, EventoUpdateView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('organizadores/', OrganizadorListView.as_view(), name='organizador_list'),
     path('organizadores/nuevo/', OrganizadorCreateView.as_view(), name='organizador_create'),
-    path('eventos/<int:pk>/editar/', EventoUpdateView.as_view(), name='evento_edit'),
+    path('eventos/', EventoListView.as_view(), name='evento_list'),
     path('eventos/nuevo/', EventoCreateView.as_view(), name='evento_create'),
     path('eventos/<int:pk>/editar/', EventoUpdateView.as_view(), name='evento_edit'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
 ]
